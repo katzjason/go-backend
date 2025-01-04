e626117cb2e186e18408673206ef18a2201fe3c0
Jason Katz <katz.jason37@gmail.com>
2025-01-04 08:43:28 -0500

    Added unit tests for Stone class methods

---
+++ src/test/java/com/example/go_backend/StoneTest.java
@@ -0,0 +1,39 @@
+package com.example.go_backend;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StoneTest {
+
+  Stone whiteStone = new Stone("White", 0,0);
+  Stone blackStone = new Stone("Black", 1,1);
+
+  @Test
+  public void getColorTest(){
+    assertEquals(whiteStone.getColor(), 'W', "Stone did not have expected color.");
+    assertEquals(blackStone.getColor(), 'B', "Stone did not have expected color.");
+  }
+
+  @Test
+  public void getXTest(){
+    assertEquals(whiteStone.getX(), 0, "Stone did not have expected x value.");
+    assertEquals(blackStone.getX(), 1, "Stone did not have expected x value.");
+  }
+
+  @Test
+  public void getYTest(){
+    assertEquals(whiteStone.getY(), 0, "Stone did not have expected y value.");
+    assertEquals(blackStone.getY(), 1, "Stone did not have expected y value.");
+  }
+
+  @Test
+  public void getLibertiesTest(){
+    assertEquals(whiteStone.getLiberties(), 4, "Stone did not have expected default liberties.");
+    whiteStone.setLiberties(3);
+    assertEquals(whiteStone.getLiberties(), 3, "Stone did not have accurately set liberties.");
+  }
+
+  
+}
