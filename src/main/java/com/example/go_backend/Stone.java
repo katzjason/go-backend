d1d13c7fd79cc98ac8efb3ca005c7bcb7f25f642
Jason Katz <katz.jason37@gmail.com>
2025-01-04 07:44:51 -0500

    Added Stone class

---
+++ src/main/java/com/example/go_backend/Stone.java
@@ -0,0 +1,46 @@
+package com.example.go_backend;
+
+public class Stone {
+  private String color;
+  private int x;
+  private int y;
+  private int liberties;
+  private Boolean isPrisoner;
+
+  // Parameterized constructor
+  public Stone(String color, int x, int y) {
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.isPrisoner = false;
+    this.liberties = 4;
+  }
+
+  public String toString() {
+    return ("Color: " + this.color +
+        "; X: " + String.valueOf(this.x) +
+        "; Y: " + String.valueOf(this.y) +
+        "; Liberties: " + String.valueOf(this.liberties)) + 
+        "; isPrisoner: " + String.valueOf(this.isPrisoner);
+  }
+
+  public char getColor() {
+    return this.color.charAt(0);
+  }
+
+  public int getX() {
+    return this.x;
+  }
+
+  public int getY() {
+    return this.y;
+  }
+
+  public int getLiberties() {
+    return this.liberties;
+  }
+
+  public void setLiberties(int liberties) {
+    this.liberties = liberties;
+  }
+}
